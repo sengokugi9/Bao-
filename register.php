c83af5fd44c3b269f2cb03e4525a9c0850c4f629
My Name Is Sen <123668758+sengokugi9@users.noreply.github.com>
2024-11-01 11:21:26 +0700

    add

---
+++ register.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Register</title>
+</head>
+<body>
+<div class="relative w-screen h-72 bg-gradient-to-r from-blue-500 to-green-500">
+  <!-- โลโก้ที่มุมซ้ายบน -->
+  <div class="absolute top-8 left-12">
+    <h1 class="text-4xl text-white">Bao</h1>
+  </div>
+  <div class="absolute top-8 right-12">
+    <div class="cicle bg-white w-10 h-10 rounded-full"></div>
+  </div>
+
+  <!-- กล่องสีขาวตรงกลาง -->
+  <div class="flex items-center justify-center w-full h-full">
+  <div class="rounded-md shadow-md w-1/2 h-32 bg-white mt-72 flex items-center">
+    <div class="nav flex justify-around items-center w-full">
+      <!-- Box Menu Item -->
+      <div class="boxmenu flex flex-col items-center justify-center bg-red-400 ">
+        <img src="genetic-algorithm-svgrepo-com.svg" class="w-16 mb-1">
+        <a href="" class="text-base text-blue-500 text-center">Test</a>
+      </div>
+      <div class="boxmenu flex flex-col items-center justify-center">
+        <img src="genetic-algorithm-svgrepo-com.svg" class="w-16 mb-1">
+        <a href="" class="text-base text-blue-500 text-center">Test</a>
+      </div>
+      <div class="boxmenu flex flex-col items-center justify-center">
+        <img src="genetic-algorithm-svgrepo-com.svg" class="w-16 mb-1">
+        <a href="" class="text-base text-blue-500 text-center">Test</a>
+      </div>
+      <div class="boxmenu flex flex-col items-center justify-center">
+        <img src="genetic-algorithm-svgrepo-com.svg" class="w-16 mb-1">
+        <a href="" class="text-base text-blue-500 text-center">Test</a>
+      </div>
+      <div class="boxmenu flex flex-col items-center justify-center">
+        <img src="genetic-algorithm-svgrepo-com.svg" class="w-16 mb-1">
+        <a href="" class="text-base text-blue-500 text-center">Test</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+</body>
+</html>
